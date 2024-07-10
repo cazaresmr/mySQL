a92b26f89014202790716c18b264f2632088856d
Michael Cazares <michaelcazares@gmail.com>
2024-07-10 12:12:02 -0700

    completed MySQL Intro exercise

---
+++ mySQLIntro.sql
@@ -0,0 +1,39 @@
+-- 1 find all products
+SELECT * FROM products;
+
+-- 2 find all products that cost $1400
+SELECT * FROM products WHERE price = 1400;
+
+-- 3 find all products that cost $11.99 or $13.99
+SELECT * FROM products WHERE price = 11.99 OR price = 13.99;
+
+-- 4 find all products that do NOT cost 11.99 - using NOT
+SELECT * FROM products WHERE NOT price = 11.99;
+
+-- 5 find all products and sort them by price from greatest to least
+SELECT * FROM products ORDER BY price DESC;
+
+-- 6 find all employees who don't have a middle initial
+SELECT * FROM employees WHERE MiddleInitial IS NULL;
+
+-- 7 find distinct product prices
+SELECT DISTINCT price FROM products;
+
+-- 8 find all employees whose first name starts with the letter 'j'
+SELECT * FROM employees WHERE FirstName LIKE 'j%';
+
+-- 9 find all Macbooks
+SELECT * FROM products WHERE name LIKE '%Macbook%';
+
+-- 10 find all products that are on sale
+SELECT * FROM products WHERE onsale = 1;
+
+-- 11 find the average price of all products
+SELECT AVG(price) AS average_price FROM products;
+
+-- 12 find all Geek Squad employees who don't have a middle initial
+SELECT * FROM employees WHERE Title = 'Geek Squad' AND middleinitial IS NULL;
+
+-- 13 find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. **Hint: Use the between keyword**
+
+SELECT * FROM products WHERE stock_level BETWEEN 500 AND 1200 ORDER BY price ASC;
